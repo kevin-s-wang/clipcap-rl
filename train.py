3119c0578e3bd87685e0bb9d36930757ee729c2e
Kevin Wang <kevin.wang10@sap.com>
2024-08-30 17:45:36 +0800

    updates

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from transformers import HfArgumentParser
 
 class ImageCaptionDataCollator(DefaultDataCollator):
     def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
-        print(features)
+        print(features.shape)
         return features
 
 @dataclass
